9b0f3d340bc8bbad36577a62867b6e7ce9eec434
GAB COL <gabycol1378@gmail.com>
2022-01-08 14:55:45 +0100

    Add Model_USE

--- Model_Use.py
+++ Model_Use.py
@@ -10,14 +10,15 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-env = UnityEnvironment(file_name="C:/Users/gabyc/Desktop/Reinforcment_TP/Value-based-methods/p1_navigation/Banana_Windows_x86_64/Banana.exe")
+env = UnityEnvironment(file_name="C:/Users/gabyc/Desktop/Reinforcment_TP/RL_Learning/Banana_Windows_x86_64/Banana.exe")
 
 # get the default brain
 brain_name = env.brain_names[0]
 brain = env.brains[brain_name]
-state_size=len(env_info.vector_observations[0])
-action_size=brain.vector_action_space_size,
 env_info = env.reset(train_mode=True)[brain_name]
+
+state_size=len(env_info.vector_observations[0])
+action_size=brain.vector_action_space_size
 
 # Import and load the model
 lettre=input("Which model you would like to use? DQN press A, DoubleDQN press B, DuelingDQN press C !")
@@ -27,7 +28,7 @@ elif lettre == "B" or lettre == "B":
   PATH = "Results/doubledqn_agent.pth"
 else:
   PATH = "Results/dueling_dqn_agent.pth"
-model = QNetwork(state_size, action_size, seed).to(device)
+model = QNetwork(state_size, action_size, seed=0).to(device)
 model.load_state_dict(torch.load(PATH))
 model.eval()
 
@@ -39,7 +40,7 @@ while True:
   state = torch.from_numpy(state).float().unsqueeze(0).to(device)
   model.eval()
   with torch.no_grad():
-    action_values = self.qnetwork_local(state)
+    action_values = model(state)
   action = np.argmax(action_values.cpu().data.numpy())
   ## Interact and grab data from the environment ##
   env_info = env.step(int(action))[brain_name]   # send the action to the environment
@@ -50,5 +51,3 @@ while True:
   state = next_state                             # roll over the state to next time step
   if done:
     break
-
-# Make the animation
